d3273df858b8f39f5539ca8b1f7da1692682fcd4
Brandon Cooper <cooperjbrandon@gmail.com>
2020-09-29 14:44:44 -0700

    query params parse + sign correctly

--- src/hooks/useQueryParams.js
+++ src/hooks/useQueryParams.js
@@ -1,7 +1,13 @@
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 
+const REPLACE_KEY = '__<<^^>>__';
 // hook to get the current query params
 export const useQueryParams = () => {
-  return queryString.parse(useLocation().search);
+  // need to do the "replace" because the .parse method turns %20 into spaces, when they should be pluses.
+  let qps = queryString.parse(useLocation().search.replace('%20', REPLACE_KEY));
+  for (let key in qps) {
+    qps[key] = qps[key].replace(REPLACE_KEY, '+');
+  }
+  return qps;
 };
