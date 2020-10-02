aa684cdca407907424654c8eca78324e6486f44c
Brandon Cooper <cooperjbrandon@gmail.com>
2020-10-01 23:40:28 -0700

    change back to pacific earth url

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ let basePath = '';
 if (process.env.REACT_APP_ENV === 'development') {
   basePath = 'http://localhost:8000';
 } else if (process.env.REACT_APP_ENV === 'staging') {
-  basePath = 'https://api-staging-collabsauce.herokuapp.com';
+  basePath = 'https://pacific-earth-58699.herokuapp.com';
 } else if (process.env.REACT_APP_ENV === 'production') {
   basePath = 'https://api.collabsauce.com';
 }
